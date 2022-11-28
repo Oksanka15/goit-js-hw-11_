b763f2c69c8664c2c0e5b00f3461cc8255601c5a
Oksanka15 <dzobaoksana1993@gmail.com>
2022-11-28 22:36:24 +0100

    new faile

---
+++ src/fetchImage.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+//fetch images
+//API key: 31600931-0a470777dbdb6f40891ab4c3c
+
+async function fetchImage(name, pages, perPage){
+    const BASE_URL = 'https://pixabay.com/api/'
+const key = 31600931-0a470777dbdb6f40891ab4c3c}
---
+++ src/index.js
@@ -0,0 +1,32 @@
+{/* <div class="photo-card">
+  <img src="" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div> */}
+
+// key - твій унікальний ключ доступу до API.
+// q - термін для пошуку. Те, що буде вводити користувач.
+// image_type - тип зображення. На потрібні тільки фотографії, тому постав значення photo.
+// orientation - орієнтація фотографії. Постав значення horizontal.
+// safesearch - фільтр за віком. Постав значення true.
+
+// webformatURL - посилання на маленьке зображення для списку карток.
+// largeImageURL - посилання на велике зображення.
+// tags - рядок з описом зображення. Підійде для атрибуту alt.
+// likes - кількість лайків.
+// views - кількість переглядів.
+// comments - кількість коментарів.
+// downloads - кількість завантажень.
+//API key: 31600931-0a470777dbdb6f40891ab4c3c
