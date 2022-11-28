4e4a12ff1166442eab914a2509063dacdbfcb35e
Oksanka15 <dzobaoksana1993@gmail.com>
2022-11-28 22:48:12 +0100

    next try

--- src/fetchImage.js
+++ src/fetchImage.js
@@ -4,4 +4,10 @@ import axios from 'axios';
 
 async function fetchImage(name, pages, perPage){
     const BASE_URL = 'https://pixabay.com/api/'
-const key = 31600931-0a470777dbdb6f40891ab4c3c}
+const key = '31600931-0a470777dbdb6f40891ab4c3c'
+try{
+    const response = await axios.get(
+        `${BASE_URL}?key=${key}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`
+    )
+}
+}
